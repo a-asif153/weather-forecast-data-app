23ab1ea37cabb55af239de45dfa1c47c37ba25fe
aasif <asifali8121@gmail.com>
2023-08-11 17:30:25 +0500

    return temp or sky conditions against input days

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -7,15 +8,11 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2023-08-08", "2023-08-09", "2023-08-10"]
-    temperatures = [10, 11, 17]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
 
 
-d, t = get_data(days)
+
+d, t = get_data(place, days, option)
 
 
 figure = px.line(x=d, y=t, labels={"x":"Date", "y":"Temperature (C)"})
-st.plotly_chart(figure)
+st.plotly_chart(figure) 
